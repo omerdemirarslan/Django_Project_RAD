3c80bc8fad047d2c858a541a308cf44cf2688751
Ömer DEMİRARSLAN <omerdemirarsln@gmail.com>
2021-04-26 23:11:36 +0300

    #RAD-00010#commen: Set The Static Files. Create Register Form Class and Register Page. Create Python Forms File For Forms. Set The Base HTML File and Create Navbar.

--- developers/models.py
+++ developers/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
 
 
 class DeveloperUser(models.Model):
     """
-    This Class Create Information Models For Developers
+    This Class Create Information Model For Developers
     """
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=25)
@@ -13,8 +14,12 @@ class DeveloperUser(models.Model):
     user_name = models.CharField(max_length=50, unique=True)
     mail_address = models.CharField(max_length=50, unique=True)
     password = models.CharField(max_length=250)
+    twitter_address = models.CharField(max_length=150, verbose_name="Twitter Profile",
+                                       help_text="Please Share Your Twitter Profile Address", blank=True, null=True)
     linkedin_address = models.CharField(max_length=150, verbose_name="Linkedin Profile",
                                         help_text="Please Share Your Linkedin Profile Address", blank=True, null=True)
+    github_address = models.CharField(max_length=150, verbose_name="Github Profile",
+                                      help_text="Please Share Your Github Profile Address", blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -24,6 +29,8 @@ class DeveloperUser(models.Model):
         ordering = ('-created_at',)
 
     def __str__(self):
-        return "User Name:{user_name}, Name:{name}, Surname:{surname}, Linkedin:{linkedin}".format(
-            user_name=self.user_name, name=self.name, surname=self.surname, linkedin=self.linkedin_address
-        )
+        return "User Name: {user_name}, Name: {name}, Surname: {surname}, Twitter: {twitter}, Linkedin: {linkedin}, " \
+               "Github: {github}".format(user_name=self.user_name, name=self.name, surname=self.surname,
+                                         twitter=self.twitter_address, linkedin=self.linkedin_address,
+                                         github=self.github_address
+                                         )
--- developers/views.py
+++ developers/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+import logging
 
-# Create your views here.
+from django.contrib import auth
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse, HttpResponseRedirect
+
+
+logger = logging.getLogger(__name__)
+
+
+@csrf_exempt
+@login_required
+def create_new_developer_user(request):
+    """
+    This Function Add The New Record Developer User In The Developer Users Model
+    :param request:
+    :return:
+    """
