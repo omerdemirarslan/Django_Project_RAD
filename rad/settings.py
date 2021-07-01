457ef74e48fe635c5bc82849aea2f3477b2f942b
Ömer DEMİRARSLAN <omerdemirarsln@gmail.com>
2021-07-02 00:24:24 +0300

    Update settings.py

--- rad/settings.py
+++ rad/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '&jwlmyq%7ivpqc5gz4!j5bx+5bt%pvls4d&$mwe59byd$20g!!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
